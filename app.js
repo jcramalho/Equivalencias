67a4eeb305a4b24b77a9aa695a7f92495e00e5f0
Jose Carlos Ramalho <jcr@di.uminho.pt>
2020-09-11 11:05:03 +0100

    Adicionei o logger.

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 var bodyParser = require('body-parser');
+var logger = require('morgan');
 
 //var index = require('./routes/index');
 var processos = require('./routes/processos');
@@ -19,6 +20,7 @@ app.set('view engine', 'pug');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
 
 app.use('/', processos);
 app.use('/processos', processos);
